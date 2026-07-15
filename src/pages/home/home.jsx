de8fa84c97b2edd951e56109e91db00e086b389a
Andrey-p-rodrigues <andrey_p_rodrigues@estudante.sesisenai.org.br>
2026-07-15 16:39:42 -0300

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -57,7 +57,7 @@ function Home() {
 
             <PokeballButton to="/pokemon/sigilyph" />
 
-            <PokeballButton to="/pokemon/Goomy" />
+            <PokeballButton to="/pokemon/goomy" />
             
 
           </div>
